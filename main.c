2dcf587c469c6993ef1a0ca7feeb2f807c8e85a4
Arifur Rahman <ar883@scarletmail.rutgers.edu>
2017-05-24 07:36:52 -0400

    indexer working and upload complete

---
+++ main.c
@@ -0,0 +1,29 @@
+/*
+Laszlo Glant & Brandon Berrios
+*/
+#include <stdio.h>
+#include <string.h>
+#include "indexer.h"
+int main(int argc, char * argv[]) {
+  
+	char* dirName;
+	char* indexName;
+	if(argc > 3)
+   	 {
+       		 printf("Error too many arguments\n");
+       		 return -1;
+   	 }
+
+   	 if(argc < 3)
+   	{
+       		 printf("Error few arguments\n");
+       		 return -1;
+  	}
+	dirName = argv[2];
+	indexName = argv[1];
+	indx = IndexCreate(indexName);
+
+	ReadDir(dirName);
+	IndexOutput(indx);
+	IndexDestroy(indx);
+	
